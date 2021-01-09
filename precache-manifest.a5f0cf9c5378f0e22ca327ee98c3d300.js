4c7039652986a768a860e84c6f6d04c9b6b4e512
ganeshapp <gapp@gapp.in>
2021-01-09 12:44:47 +0900

    deploy

---
+++ precache-manifest.a5f0cf9c5378f0e22ca327ee98c3d300.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb6a8e33dd885bd4b8c",
+    "url": "/snapcode/css/app.c7b1deb3.css"
+  },
+  {
+    "revision": "7156b00d206be14b9066",
+    "url": "/snapcode/css/chunk-6d9c2f68.3dd7bef5.css"
+  },
+  {
+    "revision": "43479c9f867ad5e7713e",
+    "url": "/snapcode/css/chunk-vendors.d9797604.css"
+  },
+  {
+    "revision": "7d264469a460036d773ae4aa37d8cd61",
+    "url": "/snapcode/image.png"
+  },
+  {
+    "revision": "803c32422f80cb17dc71f4d5e9ccac2f",
+    "url": "/snapcode/img/404.803c3242.svg"
+  },
+  {
+    "revision": "f6ce5696e634677f94343db90b9f66dd",
+    "url": "/snapcode/img/google-logo.f6ce5696.svg"
+  },
+  {
+    "revision": "f14cfb4668ff1194462229fb562ea678",
+    "url": "/snapcode/index.html"
+  },
+  {
+    "revision": "6eb6a8e33dd885bd4b8c",
+    "url": "/snapcode/js/app.9efb6806.js"
+  },
+  {
+    "revision": "7156b00d206be14b9066",
+    "url": "/snapcode/js/chunk-6d9c2f68.13237d7f.js"
+  },
+  {
+    "revision": "43479c9f867ad5e7713e",
+    "url": "/snapcode/js/chunk-vendors.124d7281.js"
+  },
+  {
+    "revision": "bb8b1fadb677e923a3653e90642b654a",
+    "url": "/snapcode/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/snapcode/robots.txt"
+  }
+]);
